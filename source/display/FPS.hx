b051b2193e17268675da6542ae7ec4a2aa6faa13
MaysLastPlay <107422651+MaysLastPlayGithub@users.noreply.github.com>
2023-02-25 10:19:20 +0200

    a bit more code

--- source/display/FPS.hx
+++ source/display/FPS.hx
@@ -16,4 +16,33 @@ import openfl.display._internal.stats.DrawCallContext;
 #if flash
 import openfl.Lib;
 #end
+#if openfl
+import openfl.system.System;
+#end
 /**The FPS class provides an easy-to-use monitor to display the current frame rate of an OpenFL project**/
+#if !openfl_debug
+@:fileXml('tags="haxe,release"')
+@:noDebug
+#end
+class FPS extends TextField
+{
+  public var currentFPS(default, null):Int;
+
+  public var bitmap:Bitmap;
+
+  public var memoryMegas:Float;
+
+	public var memoryTotal:Float;
+
+	@:noCompletion private var cacheCount:Int;
+
+	@:noCompletion private var currentTime:Float;
+
+  @:noCompletion private var times:Array<Float>;
+
+	public function new(x:Float = 15, y:Float = 15, color:Int = 0x000000)
+		{
+		super();
+
+		this.x = x;
+		this.y = y;
