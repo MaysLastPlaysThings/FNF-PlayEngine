123d7c7e343b1da0f25362e276e4ac77f5d6f1c5
MaysLastPlay <107422651+MaysLastPlayGithub@users.noreply.github.com>
2023-02-25 09:48:25 +0200

    update FPS.hx

--- source/display/FPS.hx
+++ source/display/FPS.hx
@@ -1,1 +1,19 @@
-
+package display;
+import flixel.math.FlxMath;
+import flixel.util.FlxColor;
+import openfl.Lib;
+import openfl.display.Bitmap;
+import openfl.display.BitmapData;
+import flixel.FlxG;
+import haxe.Timer;
+import openfl.events.Event;
+import openfl.text.TextField;
+import openfl.text.TextFormat;
+#if gl_stats
+import openfl.display._internal.stats.Context3DStats;
+import openfl.display._internal.stats.DrawCallContext;
+#end
+#if flash
+import openfl.Lib;
+#end
+/**The FPS class provides an easy-to-use monitor to display the current frame rate of an OpenFL project**/
