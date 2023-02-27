0385fd305186b8f4d634061f3c8b109b63b2ead8
MaysLastPlay <107422651+MaysLastPlayGithub@users.noreply.github.com>
2023-02-27 19:50:56 +0200

    Fixes

--- source/ui/OptionsState.hx
+++ source/ui/OptionsState.hx
@@ -2,6 +2,8 @@ package ui;
 
 import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
 import haxe.ds.EnumValueMap;
 
 class OptionsState extends MusicBeatState
