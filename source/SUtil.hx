3caea49f6887c2e3a505a6a8fc5fa12a125d71a2
MaysLastPlay <107422651+MaysLastPlayGithub@users.noreply.github.com>
2023-02-27 15:09:30 +0200

    oops, my bad

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -9,7 +9,7 @@ import lime.app.Application;
 import openfl.events.UncaughtErrorEvent;
 import openfl.utils.Assets as OpenFlAssets;
 import openfl.Lib;
-import FlxG;
+import flixel.FlxG;
 import haxe.CallStack.StackItem;
 import haxe.CallStack;
 import haxe.io.Path;
