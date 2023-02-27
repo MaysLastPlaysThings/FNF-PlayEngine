0e53d59d5804fa85a69188d778f5b0d74a74b64e
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2023-02-27 18:01:46 +0300

    Update OptionsState.hx

--- source/ui/OptionsState.hx
+++ source/ui/OptionsState.hx
@@ -43,7 +43,15 @@ class OptionsState extends MusicBeatState
 			setPage(Controls);
 		}
 		currentPage.enabled = false;
-                #if mobile addVirtualPad(LEFT_FULL, A_B); #end
+                #if mobile addVirtualPad(LEFT_FULL, A_B_C); #end
+
+                #if mobile
+		var xd:FlxText = new FlxText(10, 14, 0, 'Press C to customize your android controls', 16);
+		xd.setFormat(Paths.font('vcr.ttf'), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		xd.borderSize = 2.4;
+		xd.scrollFactor.set();
+		add(xd);
+		#end
 		super.create();
 	}
 
