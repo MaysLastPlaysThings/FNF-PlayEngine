a6ce9c97ecb9a21a875b68b7cf1b457fd753025f
MaysLastPlay <107422651+MaysLastPlayGithub@users.noreply.github.com>
2023-02-28 11:45:45 +0200

    update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -65,6 +65,10 @@ class TitleState extends MusicBeatState
 
 	override public function create():Void
 	{
+		#if android
+		FlxG.android.preventDefaultKeys = [BACK];
+		#end
+
 		#if polymod
 		polymod.Polymod.init({modRoot: "mods", dirs: ['introMod'], framework: OPENFL});
 		#end
