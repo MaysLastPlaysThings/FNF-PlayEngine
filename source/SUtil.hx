67e9d312d74649cc445f66a30bcff3bde0b2b5be
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-04-15 16:22:51 +0300

    update source/SUtil.hx

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -59,7 +59,7 @@ class SUtil
 			if (!FileSystem.exists(SUtil.getPath() + 'assets') && !FileSystem.exists(SUtil.getPath() + 'mods'))
 			{
 				SUtil.applicationAlert('Uncaught Error :(!', "Whoops, seems you didn't extract the files from the .APK!\nPlease watch the tutorial by pressing OK.");
-				Flx.openURL('https://youtu.be/zjvkTmdWvfU');
+				FlxG.openURL('https://youtu.be/zjvkTmdWvfU');
 				System.exit(0);
 			}
 			else
@@ -67,14 +67,14 @@ class SUtil
 				if (!FileSystem.exists(SUtil.getPath() + 'assets'))
 				{
 					SUtil.applicationAlert('Uncaught Error :(!', "Whoops, seems you didn't extract the assets/assets folder from the .APK!\nPlease watch the tutorial by pressing OK.");
-					Flx.openURL('https://youtu.be/zjvkTmdWvfU');
+					FlxG.openURL('https://youtu.be/zjvkTmdWvfU');
 					System.exit(0);
 				}
 
 				if (!FileSystem.exists(SUtil.getPath() + 'mods'))
 				{
 					SUtil.applicationAlert('Uncaught Error :(!', "Whoops, seems you didn't extract the assets/mods folder from the .APK!\nPlease watch the tutorial by pressing OK.");
-					Flx.openURL('https://youtu.be/zjvkTmdWvfU');
+					FlxG.openURL('https://youtu.be/zjvkTmdWvfU');
 					System.exit(0);
 				}
 			}
